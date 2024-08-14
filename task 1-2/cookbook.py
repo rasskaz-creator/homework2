78a1aef258f8636e9ceda6a7d00db0741d4c994c
sunset <9194113206@mail.ru>
2024-08-14 17:09:02 +0000

    Updated

--- task 1-2/cookbook.py
+++ task 1-2/cookbook.py
@@ -1,46 +1,55 @@
 # задача 1
+def open_cookbook(filename):
+    with open(filename, encoding = 'utf-8') as file:
+        lines = file.readlines()
+        
+    return lines
+      
 
-with open("recipes.txt", encoding = 'utf-8') as file:
-    lines = file.readlines()
-   
-cookbook = {}
-        
-index = 0
-while index < len(lines):
-    dish_name = lines[index].strip()
-    index += 1
-    num_ingredients = int(lines[index].strip())
-    index += 1
+def parse_cookbook(lines):
+    cookbook = {}
     
-    ingredients = []
-    
-    for ingredient in range(num_ingredients):
-        split_ingregients = lines[index].strip().split(' | ')
-        ingredient_dict = {
-            'ingredient_name' : split_ingregients[0],
-            'quantity' : split_ingregients[1],
-            'measure' : split_ingregients[2]
-        }
-        
-        ingredients.append(ingredient_dict)
+    index = 0
+    while index < len(lines):
+        dish_name = lines[index].strip()
+        index += 1
+        num_ingredients = int(lines[index].strip())
         index += 1
         
-    cookbook[dish_name] = ingredients
+        ingredients = []
+        
+        for row in range(num_ingredients):
+            split_ingregients = lines[index].strip().split(' | ')
+            ingredient_dict = {
+                'ingredient_name' : split_ingregients[0],
+                'quantity' : int(split_ingregients[1]),
+                'measure' : split_ingregients[2]
+                }
+            
+            ingredients.append(ingredient_dict)
+            index += 1
+            
+        cookbook[dish_name] = ingredients
+        
+        if index < len(lines) and lines[index].strip() == '':
+            index += 1
+            
+    return cookbook
+
     
-    if index < len(lines) and lines[index].strip() == '':
-        index += 1
-    
-#print(cookbook)
+#print(parse_cookbook(open_cookbook("recipes.txt")))
 
 # задача 2
 
 def get_shop_list_by_dishes(dishes: list, person_count: int):
+    
+    recipes = parse_cookbook(open_cookbook("recipes.txt"))
     result = {}
-    for dish_name in cookbook:
-        if dish_name in dishes:
-            for ingredient in cookbook[dish_name]:
+    for dish_name in dishes:
+        if dish_name in recipes:
+            for ingredient in recipes[dish_name]:
                 ingredient_name = ingredient['ingredient_name']
-                quantity = int(ingredient['quantity']) * person_count
+                quantity = ingredient['quantity'] * person_count
                 measure = ingredient['measure']
                 
                 if ingredient_name in result:
@@ -50,3 +59,4 @@ def get_shop_list_by_dishes(dishes: list, person_count: int):
     return result
 
 print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+print(get_shop_list_by_dishes(['Омлет', 'Омлет'], 2))
